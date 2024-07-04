38cf6e901b637abc584eb7075949689869095010
pajo-odoo <pajo@odoo.com>
2024-07-04 19:00:59 +0530

    Fetched trailer of movie + video components

---
+++ src/utils/constants.js
@@ -0,0 +1,10 @@
+export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png";
+export const USER_AVATAR = "https://avatars.githubusercontent.com/u/157008692?v=4";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NThmYmMyNzQ5Zjk0ZWIwZjJlNTgzMzJkNzZkY2QwYyIsIm5iZiI6MTcxOTk5ODYzOS4wNDAxODcsInN1YiI6IjY2ODUxM2VjNWQ2NzViZWQ5MWQxYmViMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.F52LhyBs17RLn7j3XmXE7AOjk1Nuk4k4mGzP228oOfg'
+    }
+};
