b764bce046311c545b79214e65fbb3ce86043d6b
pajo-odoo <pajo@odoo.com>
2024-07-31 17:50:41 +0530

    Added upcoming, top-rated movies

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,9 +9,9 @@ const SecondaryContainer = () => {
             <div className="bg-black">
                 <div className="-mt-60 pl-12 relative z-20">
                     <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
-                    <MovieList title={"Trending"} movies={movies.nowPlayingMovies} />
+                    <MovieList title={"Up Coming Movies"} movies={movies.upcomingMovies} />
                     <MovieList title={"Popular"} movies={movies.popularMovies} />
-                    <MovieList title={"Upcoming Movies"} movies={movies.nowPlayingMovies} />
+                    <MovieList title={"Top Rated Movies"} movies={movies.topRatedMovies} />
                     <MovieList title={"Horror"} movies={movies.nowPlayingMovies} />
                 </div>
             </div>
